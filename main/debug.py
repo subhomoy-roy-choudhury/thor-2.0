ddd347375277e4dac98b050f6427b4fdfb4669e9
Subhomoy Roy Choudhury <subhomoyrchoudhury@gmail.com>
2021-03-28 20:41:30 +0530

    Added threading 1

--- main/debug.py
+++ main/debug.py
@@ -1,28 +1,10 @@
 import datetime
 import os
-import threading
 from threading import Thread
 from multiprocessing import Process
-from concurrent.futures import ThreadPoolExecutor
 import platform
 import gtts
 from playsound import playsound
-import concurrent.futures
-import psutil
-
-def checkIfProcessRunning(processName):
-    '''
-    Check if there is any running process that contains the given name processName.
-    '''
-    #Iterate over the all the running process
-    for proc in psutil.process_iter():
-        try:
-            # Check if process name contains the given name string.
-            if processName.lower() in proc.name().lower():
-                return True
-        except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
-            pass
-    return False;
 
 def speak(text):
     file = "hola.mp3"
